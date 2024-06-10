1bfee79213a92f4d3bd6923259eec956a31d4ee9
dimazaicev88 <dimazaicev88@gmail.com>
2024-06-10 16:58:13 +0300

    fix changes

--- internal/models/adv.go
+++ internal/models/adv.go
@@ -3,14 +3,24 @@ package models
 import (
 	"bitrix-statistic/internal/session"
 	"bitrix-statistic/internal/storage"
+	"encoding/base64"
 	"net/url"
+	"strings"
 )
 
 type AdvModel struct {
-	storage storage.Storage
+	storage     storage.Storage
+	optionModel *OptionModel
 }
 
-func (am AdvModel) SetAdv(fullRequestUri string, phpSession *session.Session, referringSite string) error {
+func NewAdvModel(storage storage.Storage, optionModel *OptionModel) *AdvModel {
+	return &AdvModel{
+		storage:     storage,
+		optionModel: optionModel,
+	}
+}
+
+func (am AdvModel) SetAdv(phpSession *session.Session, fullRequestUri, openstat, referringSite string) error {
 	//$err_mess = "File: " . __FILE__ . "<br>Line: ";
 	//stat_session_register("SESS_ADV_ID") // ID рекламной кампании
 	//$DB = CDatabase::GetModuleConnection('statistic');
@@ -47,30 +57,32 @@ func (am AdvModel) SetAdv(fullRequestUri string, phpSession *session.Session, re
 		if phpSession.Get("referer1") != "" || phpSession.Get("referer2") != "" {
 			listAdv, ref1, ref2, err = am.FindByReferer(phpSession.Get("referer1"), phpSession.Get("referer2"))
 		}
-		//Handle Openstat if enabled
-		if COption::GetOptionString("statistic", "OPENSTAT_ACTIVE") == = "Y" && $_REQUEST["_openstat"] < > '') {
-			$openstat = $_REQUEST["_openstat"]
-			if mb_strpos($openstat, ";") == = false)
-			$openstat = base64_decode($openstat)
-			$openstat = explode(";", $openstat)
-		CAdv::SetByReferer(
-				trim(str_replace(
-					array("#service-name#", "#campaign-id#", "#ad-id#", "#source-id#"),
-			$openstat,
-				COption::GetOptionString("statistic", "OPENSTAT_R1_TEMPLATE")
-			)),
-			trim(str_replace(
-				array("#service-name#", "#campaign-id#", "#ad-id#", "#source-id#"),
-			$openstat,
-				COption::GetOptionString("statistic", "OPENSTAT_R2_TEMPLATE")
-			)),
-			$arrADV, $ref1, $ref2
-			)
-		}
-		$arrADV = array_unique($arrADV)
+		//Handle Openstat if enabled TODO Доделать
+		//if am.optionModel.Get("OPENSTAT_ACTIVE") == "Y" && len(openstat) >0 {
+		//	if strings.Contains(openstat,";")== false{
+		//		result, err := base64.StdEncoding.DecodeString(openstat)
+		//		if err != nil {
+		//			return err
+		//		}
+		//		values:=strings.Split(string(result), ";")
+		//	}
+		//am.FindByReferer(
+		//		trim(str_replace(
+		//			array("#service-name#", "#campaign-id#", "#ad-id#", "#source-id#"),
+		//	$openstat,
+		//		COption::GetOptionString("statistic", "OPENSTAT_R1_TEMPLATE")
+		//	)),
+		//	trim(str_replace(
+		//		array("#service-name#", "#campaign-id#", "#ad-id#", "#source-id#"),
+		//	$openstat,
+		//		COption::GetOptionString("statistic", "OPENSTAT_R2_TEMPLATE")
+		//	)),
+		//	$arrADV, $ref1, $ref2
+		//	)
+		//}
 
 		// если было выявлено более одной рекламной кампании подходящей под условия то
-		if count($arrADV) > 1) {
+		if len(listAdv) > 1 {
 			// выберем рекламную кампанию по наивысшему приоритету (либо по наивысшему ID)
 			$str = implode(",", $arrADV)
 			$strSql = "SELECT ID, REFERER1, REFERER2 FROM b_stat_adv WHERE ID in ($str) ORDER BY PRIORITY desc, ID desc"
@@ -156,56 +168,58 @@ func (am AdvModel) FindByByDomainSearcher(host string) ([]int, string, string, e
 
 func (am AdvModel) FindByReferer(referer1, referer2 string) ([]int, string, string, error) {
 
-   // lookup campaign with referer1 and referer2
-	$referer1 = trim($referer1);
-	$referer1_sql = $referer1 <> '' ? "REFERER1='" . $DB->ForSql($referer1, 255) . "'" : "(REFERER1 is null or " . $DB->Length("REFERER1") . "=0)";
-	$referer2 = trim($referer2);
-	$referer2_sql = $referer2 <> '' ? "REFERER2='" . $DB->ForSql($referer2, 255) . "'" : "(REFERER2 is null or " . $DB->Length("REFERER2") . "=0)";
+	// lookup campaign with referer1 and referer2
+	$referer1 = trim($referer1)
+	$referer1_sql = $referer1 < > '' ? "REFERER1='". $DB- > ForSql($referer1, 255)."'" : "(REFERER1 is null or ". $DB- > Length("REFERER1").
+	"=0)"
+	$referer2 = trim($referer2)
+	$referer2_sql = $referer2 < > '' ? "REFERER2='". $DB- > ForSql($referer2, 255)."'" : "(REFERER2 is null or ". $DB- > Length("REFERER2").
+	"=0)"
 
 	sql := `
 	SELECT 	ID, REFERER1, REFERER2
 	FROM adv
 	WHERE  REFERER1=? and REFERER2=?`
 
-	$found = false;
-	while ($wr = $w->Fetch()) {
-		$found = true;
+	$found = false
+	while($wr = $w- > Fetch()) {
+		$found = true
 		// return with parameters
-		$arrADV[] = intval($wr["ID"]);
-		$ref1 = $wr["REFERER1"];
-		$ref2 = $wr["REFERER2"];
+		$arrADV[] = intval($wr["ID"])
+		$ref1 = $wr["REFERER1"]
+		$ref2 = $wr["REFERER2"]
 	}
 
-	if (!$found) {
-		$NA = "";
-		if (COption::GetOptionString("statistic", "ADV_NA") == "Y") {
-			$NA_1 = COption::GetOptionString("statistic", "AVD_NA_REFERER1");
-			$NA_2 = COption::GetOptionString("statistic", "AVD_NA_REFERER2");
-			if (($NA_1 <> '' || $NA_2 <> '') && $referer1 == $NA_1 && $referer2 == $NA_2)
-			$NA = "Y";
+	if !$found) {
+		$NA = ""
+		if COption::GetOptionString("statistic", "ADV_NA") == "Y") {
+			$NA_1 = COption::GetOptionString("statistic", "AVD_NA_REFERER1")
+			$NA_2 = COption::GetOptionString("statistic", "AVD_NA_REFERER2")
+			if ($NA_1 < > '' || $NA_2 < > '') && $referer1 == $NA_1 && $referer2 == $NA_2)
+			$NA = "Y"
 		}
 
-		if ((COption::GetOptionString("statistic", "ADV_AUTO_CREATE") == "Y") || ($NA == "Y")) {
-	if (COption::GetOptionString("statistic", "REFERER_CHECK") == "Y") {
-	$bGoodR = preg_match("/^([0-9A-Za-z_:;.,-])*$/", $referer1);
-	if ($bGoodR)
-	$bGoodR = preg_match("/^([0-9A-Za-z_:;.,-])*$/", $referer2);
-	} else {
-	$bGoodR = true;
-	}
+		if COption::GetOptionString("statistic", "ADV_AUTO_CREATE") == "Y") || ($NA == "Y")) {
+if (COption::GetOptionString("statistic", "REFERER_CHECK") == "Y") {
+$bGoodR = preg_match("/^([0-9A-Za-z_:;.,-])*$/", $referer1);
+if ($bGoodR)
+$bGoodR = preg_match("/^([0-9A-Za-z_:;.,-])*$/", $referer2);
+} else {
+$bGoodR = true;
+}
 
-	if ($bGoodR) {
-	// add new advertising campaign
-	$arFields = array(
-	"REFERER1" => $referer1 <> '' ? "'" . $DB->ForSql($referer1, 255) . "'" : "null",
-	"REFERER2" => $referer2 <> '' ? "'" . $DB->ForSql($referer2, 255) . "'" : "null",
-	"DATE_FIRST" => $DB->GetNowFunction(),
-	"DATE_LAST" => $DB->GetNowFunction(),
-	);
-	$arrADV[] = $DB->Insert("b_stat_adv", $arFields, $err_mess . __LINE__);
-	$ref1 = $referer1;
-	$ref2 = $referer2;
-	}
-	}
+if ($bGoodR) {
+// add new advertising campaign
+$arFields = array(
+"REFERER1" = > $referer1 <> '' ? "'".$DB->ForSql($referer1, 255)."'": "null",
+"REFERER2" = > $referer2 <> '' ? "'".$DB->ForSql($referer2, 255)."'": "null",
+"DATE_FIRST" = > $DB->GetNowFunction(),
+"DATE_LAST" = > $DB->GetNowFunction(),
+);
+$arrADV[] = $DB->Insert("b_stat_adv", $arFields, $err_mess.__LINE__);
+$ref1 = $referer1;
+$ref2 = $referer2;
+}
+}
 }
 }
