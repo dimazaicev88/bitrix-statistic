c118d1f9e7c4e52a97f0d0bab79e48f951e34d3b
zajtsev <dimazaicev88@gmail.com>
2024-10-23 16:28:45 +0300

    fix changes

--- internal/models/path_adv.go
+++ internal/models/path_adv.go
@@ -22,16 +22,19 @@ func NewPathAdv(ctx context.Context, chClient driver.Conn) *PathAdv {
 
 func (pa PathAdv) Add(pathAdv entitydb.PathAdv) error {
 	return pa.chClient.Exec(pa.ctx,
-		`INSERT INTO path_adv (adv_uuid, path_id, date_stat, counter, counter_back, counter_full_path, counter_full_path_back, steps, sign, version)
+		`INSERT INTO path_adv (advUuid, pathId, dateStat, counter, counterBack, 
+                      counterFullPath, counterFullPathBack, steps, sign, version)
 			VALUES (?,?,curdate(),?,?,?,?,?,?,?)`,
-		pathAdv.AdvUuid, pathAdv.PathId, pathAdv.Counter, pathAdv.CounterBack, pathAdv.CounterFullPath, pathAdv.CounterFullPathBack, pathAdv.Steps, pathAdv.Sign,
+		pathAdv.AdvUuid, pathAdv.PathId, pathAdv.Counter, pathAdv.CounterBack, pathAdv.CounterFullPath,
+		pathAdv.CounterFullPathBack, pathAdv.Steps, pathAdv.Sign,
 		pathAdv.Version,
 	)
 }
 
 func (pa PathAdv) FindByPathUuid(pageId int32, dateStat string) (entitydb.PathAdv, error) {
 	var pathAdv entitydb.PathAdv
-	err := pa.chClient.QueryRow(pa.ctx, `SELECT * FROM path_adv WHERE path_id = ? and date_stat=?`, pageId, dateStat).Scan(&pathAdv)
+	err := pa.chClient.QueryRow(pa.ctx, `SELECT * FROM path_adv WHERE pathId = ? and dateStat=?`,
+		pageId, dateStat).Scan(&pathAdv)
 	if err != nil && errors.Is(err, sql.ErrNoRows) == false {
 		return entitydb.PathAdv{}, err
 	}
@@ -40,7 +43,8 @@ func (pa PathAdv) FindByPathUuid(pageId int32, dateStat string) (entitydb.PathAd
 
 func (pa PathAdv) Update(oldValue entitydb.PathAdv, newValue entitydb.PathAdv) error {
 	err := pa.chClient.Exec(pa.ctx,
-		`INSERT INTO path_adv (adv_uuid, path_id, date_stat, counter, counter_back, counter_full_path, counter_full_path_back, steps, sign, version)
+		`INSERT INTO path_adv (advUuid, pathId, dateStat, counter, counterBack, counterFullPath, counterFullPathBack, 
+                      steps, sign, version)
 			VALUES (?,?,curdate(),?,?,?,?,?,?,?)`,
 		oldValue.AdvUuid, oldValue.Counter, oldValue.CounterBack, oldValue.CounterFullPath, oldValue.CounterFullPathBack, oldValue.Steps, oldValue.Sign*-1, oldValue.Version,
 	)
@@ -49,7 +53,8 @@ func (pa PathAdv) Update(oldValue entitydb.PathAdv, newValue entitydb.PathAdv) e
 	}
 
 	err = pa.chClient.Exec(pa.ctx,
-		`INSERT INTO path_adv (adv_uuid, path_id, date_stat, counter, counter_back, counter_full_path, counter_full_path_back, steps, sign, version)
+		`INSERT INTO path_adv (advUuid, pathId, dateStat, counter, counterBack, counterFullPath, counterFullPathBack,
+                      steps, sign, version)
 			VALUES (?,?,curdate(),?,?,?,?,?,?,?)`,
 		newValue.AdvUuid, newValue.Counter, newValue.CounterBack, newValue.CounterFullPath, newValue.CounterFullPathBack, newValue.Steps, 1, newValue.Version+1,
 	)
@@ -61,7 +66,8 @@ func (pa PathAdv) Update(oldValue entitydb.PathAdv, newValue entitydb.PathAdv) e
 
 func (pa PathAdv) FindByPageAndAdvUuid(pathId int32, advUuid string) (entitydb.PathAdv, error) {
 	var pathAdv entitydb.PathAdv
-	err := pa.chClient.QueryRow(pa.ctx, `SELECT * FROM path_adv WHERE  path_id= ? and adv_uuid=?`, pathId, advUuid).Scan(&pathAdv)
+	err := pa.chClient.QueryRow(pa.ctx, `SELECT * FROM path_adv WHERE  pathId= ? and advUuid=?`,
+		pathId, advUuid).Scan(&pathAdv)
 	if err != nil {
 		return entitydb.PathAdv{}, err
 	}
